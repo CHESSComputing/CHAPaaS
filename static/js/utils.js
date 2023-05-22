38828a82c75d0e7bc2398163bbb59879481bca46
Valentin Kuznetsov <vkuznet@gmail.com>
2023-05-22 09:47:40 -0400

    add js, styles for submenu

---
+++ static/js/utils.js
@@ -0,0 +1,22 @@
+function HideTag(tag) {
+    var id=document.getElementById(tag);
+    if (id) {
+        id.className="hide";
+    }
+}
+function ShowTag(tag) {
+    var id=document.getElementById(tag);
+    if (id) {
+        id.className="show";
+    }
+}
+function FlipTag(tag) {
+    var id=document.getElementById(tag);
+    if (id) {
+        if  (id.className == "show") {
+            id.className="hide";
+        } else {
+            id.className="show";
+        }
+    }
+}
