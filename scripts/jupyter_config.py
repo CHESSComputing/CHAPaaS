dc7f398a0d4c73b998b729f5e9e5816dcc6fd84e
Valentin Kuznetsov <vkuznet@gmail.com>
2023-05-26 10:05:33 -0400

    Adjust port

--- scripts/jupyter_config.py
+++ scripts/jupyter_config.py
@@ -6,7 +6,7 @@ c.NotebookApp.tornado_settings = {
 }
 c.NotebookApp.token = "47e67734f6221fec0f18fab5c501c8bef133b14195fdbc08"
 c.NotebookApp.open_browser = False
-c.NotebookApp.port = 8888
+c.NotebookApp.port = 18888
 
 c.JupyterHub.tornado_settings = {
     'headers': {
