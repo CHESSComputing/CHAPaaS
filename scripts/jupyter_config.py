c43ae129c3581a2ec4075647f5233fe5cb68f340
Valentin Kuznetsov <vkuznet@gmail.com>
2023-05-26 09:53:07 -0400

    update

--- scripts/jupyter_config.py
+++ scripts/jupyter_config.py
@@ -1,8 +1,11 @@
+# see https://jupyter-notebook.readthedocs.io/en/stable/public_server.html
 c.NotebookApp.tornado_settings = {
     "headers": {
         "Content-Security-Policy": "frame-ancestors 'self' http://localhost:8181"
     }
 }
+c.NotebookApp.token = "47e67734f6221fec0f18fab5c501c8bef133b14195fdbc08"
+c.NotebookApp.open_browser = False
 
 c.JupyterHub.tornado_settings = {
     'headers': {
