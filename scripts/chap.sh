0a52d436c9e9a68c02d01d162ae5859541ccbeeb
Valentin Kuznetsov <vkuznet@gmail.com>
2023-05-26 18:20:46 -0400

    Add python version

--- scripts/chap.sh
+++ scripts/chap.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
+pyver=`python -V | awk '{split($2,a,"."); print ""a[1]"."a[2]""}'`
 config=$1
 chapdir=$2
 userdir=$3
 #echo "+++ config $config"
 #echo "+++ chapdir $chapdir"
 #echo "+++ userdir $userdir"
-export PYTHONPATH=$chapdir/lib/python*/site-packages/:$userdir
+export PYTHONPATH=$chapdir/lib/python$pyver/site-packages:$chapdir/lib64/python$pyver/site-packages:$userdir
 #python -V
 #python -c "from users import *; print(UserProcessor)"
 #echo "#### start CHAP with $PYTHONPATH"
