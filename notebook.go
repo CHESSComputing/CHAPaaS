b0b73a4c94e5b9747135ec485d26b944c5349023
Valentin Kuznetsov <vkuznet@gmail.com>
2023-05-29 10:30:28 -0400

    code refactoring

--- notebook.go
+++ notebook.go
@@ -11,13 +11,27 @@ import (
 	"time"
 )
 
-/*
- * The JupyterRoot defines top level directory where we run Jupyter app
- * Within this area we must create /users where we'll store each individual
- * user notebooks. Therefore, in a code we use /api/contents/users API
- * which includes /users path which should exist in JupyterRoot
- * Create() api of Notebook struct will properly creats /users area within JupyterRoot area
- */
+// NotebookRecord defines notebook record structure
+type NotebookRecord struct {
+	Name         string `json:"name"`
+	Path         string `json:"path"`
+	LastModified string `json:"last_modified"`
+	Created      string `json:"created"`
+	Content      NotebookContent
+}
+
+// NotebookContent defines notebook content structure
+type NotebookContent struct {
+	Cells []Cell
+}
+
+// Cell defines notebook cell structure
+type Cell struct {
+	CellType         string
+	ExecutionCounter int
+	Id               string
+	Source           string
+}
 
 // Notebook represents jupyter notebook object
 type Notebook struct {
@@ -28,6 +42,13 @@ type Notebook struct {
 	FileName string // notebook file name
 }
 
+/*
+ * The JupyterRoot defines top level directory where we run Jupyter app
+ * Within this area we must create /users where we'll store each individual
+ * user notebooks. Therefore, in a code we use /api/contents/users API
+ * which includes /users path which should exist in JupyterRoot
+ * Create() api of Notebook struct will properly creats /users area within JupyterRoot area
+ */
 // Create creates notebook user area and notebook file
 func (n *Notebook) Create() error {
 	// ensure that new user's area exists under JupyterRoot
@@ -40,15 +61,25 @@ func (n *Notebook) Create() error {
 		return err
 	}
 
-	// create notebook file
+	// check if JupyterRoot/users/<user> area has the notebook file name
+	rurl := fmt.Sprintf("%s/api/contents/users/%s/%s", n.Host, n.User, n.FileName)
+	rec, err := notebookCall("GET", rurl, n.Token, nil)
+	if Config.Verbose > 0 {
+		log.Printf("HTTP GET %s, it has %+v, error=%v", rurl, rec, err)
+	}
+	if err == nil && rec.Created != "" {
+		// we have existing notebook file
+		return nil
+	}
+
+	// create notebook file if it does not exist
 	// https://jupyter-server.readthedocs.io/en/latest/developers/rest-api.html
 	var jsonData = []byte(`{"type": "notebook"}`)
-	//     rurl := fmt.Sprintf("%s/api/contents/%s/%s", n.Host, n.Root, n.User)
-	rurl := fmt.Sprintf("%s/api/contents/users/%s", n.Host, n.User)
+	rurl = fmt.Sprintf("%s/api/contents/users/%s", n.Host, n.User)
 	if Config.Verbose > 0 {
-		log.Printf("jupyter request HTTP POST %s %+v", rurl, string(jsonData))
+		log.Printf("HTTP POST %s %+v", rurl, string(jsonData))
 	}
-	rec, err := notebookCall("POST", rurl, n.Token, bytes.NewBuffer(jsonData))
+	rec, err = notebookCall("POST", rurl, n.Token, bytes.NewBuffer(jsonData))
 	if Config.Verbose > 0 {
 		log.Printf("jupyter response %+v, error %v", rec, err)
 	}
@@ -57,7 +88,6 @@ func (n *Notebook) Create() error {
 
 // Capture fetches content of notebook file
 func (n *Notebook) Capture() (NotebookRecord, error) {
-	//     rurl := fmt.Sprintf("%s/api/contents/%s/%s/%s", n.Host, n.Root, n.User, n.FileName)
 	rurl := fmt.Sprintf("%s/api/contents/users/%s/%s", n.Host, n.User, n.FileName)
 	rec, err := notebookCall("GET", rurl, n.Token, nil)
 	return rec, err
@@ -90,24 +120,4 @@ func notebookCall(method, rurl, token string, body io.Reader) (NotebookRecord, e
 	defer resp.Body.Close()
 	err = json.NewDecoder(resp.Body).Decode(&rec)
 	return rec, nil
-}
-
-/*
-{"name": "Untitled.ipynb", "path": "Untitled.ipynb", "last_modified": "2023-05-19T13:55:49.526770Z", "created": "2023-05-19T13:55:49.526770Z", "content": {"cells": [{"cell_type": "code", "execution_count": 1, "id": "4dc583a6", "metadata": {"trusted": false}, "outputs": [], "source": "a=1"}, {"cell_type": "code", "execution_count": 2, "id": "a5ae958c", "metadata": {"trusted": false}, "outputs": [{"name": "stdout", "output_type": "stream", "text": "1\n"}], "source": "print(a)"}, {"cell_type": "code", "execution_count": null, "id": "e7fdf954", "metadata": {"trusted": false}, "outputs": [], "source": ""}], "metadata": {"kernelspec": {"display_name": "Python 3 (ipykernel)", "language": "python", "name": "python3"}, "language_info": {"codemirror_mode": {"name": "ipython", "version": 3}, "file_extension": ".py", "mimetype": "text/x-python", "name": "python", "nbconvert_exporter": "python", "pygments_lexer": "ipython3", "version": "3.11.3"}}, "nbformat": 4, "nbformat_minor": 5}, "format": "json", "mimetype": null, "size": 989, "writable": true, "type": "notebook"}
-*/
-type NotebookRecord struct {
-	Name         string `json:"name"`
-	Path         string `json:"path"`
-	LastModified string `json:"last_modified"`
-	Created      string `json:"created"`
-	Content      NotebookContent
-}
-type NotebookContent struct {
-	Cells []Cell
-}
-type Cell struct {
-	CellType         string
-	ExecutionCounter int
-	Id               string
-	Source           string
 }
