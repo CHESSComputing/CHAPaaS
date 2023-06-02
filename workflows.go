99b805c6fee776327095f083a45ec73bc225f0f6
Valentin Kuznetsov <vkuznet@gmail.com>
2023-06-02 11:06:28 -0400

    Update data structure

--- workflows.go
+++ workflows.go
@@ -4,15 +4,16 @@ import "encoding/json"
 
 // Workflow define CHAP workflow record
 type Workflow struct {
-	MetaData    map[string]interface{} `json:"meta_data"`   // meta-data information about ML model
-	Name        string                 `json:"name"`        // workflow name
-	Type        string                 `json:"type"`        // workflow type
-	Group       string                 `json:"group"`       // workflow group
-	Version     string                 `json:"version"`     // workflow version
-	Description string                 `json:"description"` // workflow description
-	Reference   string                 `json:"reference"`   // reference URL
-	UserName    string                 `json:"user_name"`   // user name
-	UserID      string                 `json:"user_id"`     // user id
+	MetaData    map[string]interface{} `json:"meta_data",yaml:"meta_data"`     // meta-data information about ML model
+	Name        string                 `json:"name",yaml:"name"`               // workflow name
+	Directory   string                 `json:"directory",yaml:"directory"`     // workflow directory
+	Type        string                 `json:"type",yaml:"type"`               // workflow type
+	Group       string                 `json:"group",yaml:"group"`             // workflow group
+	Version     string                 `json:"version",yaml:"version"`         // workflow version
+	Description string                 `json:"description",yaml:"description"` // workflow description
+	Reference   string                 `json:"reference",yaml:"reference"`     // reference URL
+	UserName    string                 `json:"user_name",yaml:"user_name"`     // user name
+	UserID      string                 `json:"user_id",yaml:"user_id"`         // user id
 }
 
 // ToJSON provides string representation of Record
