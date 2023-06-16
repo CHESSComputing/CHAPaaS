5b6098fccb7737b8b44c031ec8055fe788ceefcf
Valentin Kuznetsov <vkuznet@gmail.com>
2023-06-16 13:03:10 -0400

    New skeleton script for publishing user codebase

---
+++ scripts/publish.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+notebook=$1
+# TODO: we need to implement business logic of this script
+# - check/get access token to remote github/gitlab repo
+# - check/create new user area
+# - commit user notebook code to its area
+echo "Publish $notebook"
