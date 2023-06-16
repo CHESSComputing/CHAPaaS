8a89e4b7c275d1bac956168a27879d8ce6ce233d
Valentin Kuznetsov <vkuznet@gmail.com>
2023-06-16 10:39:09 -0400

    Add support for profile option

--- scripts/chap.sh
+++ scripts/chap.sh
@@ -3,6 +3,7 @@ pyver=`python -V | awk '{split($2,a,"."); print ""a[1]"."a[2]""}'`
 config=$1
 chapdir=$2
 userdir=$3
+profile=$4
 #echo "+++ config $config"
 #echo "+++ chapdir $chapdir"
 #echo "+++ userdir $userdir"
@@ -11,4 +12,8 @@ export PYTHONPATH=$PYTHONPATH:$chapdir/venv/lib/python$pyver/site-packages
 #python -V
 #python -c "from users import *; print(UserProcessor)"
 #echo "#### start CHAP with $PYTHONPATH"
-$chapdir/bin/CHAP --config $config 2>&1
+if [ -n "$profile" ]; then
+    $chapdir/bin/CHAP --profile --config $config 2>&1
+else
+    $chapdir/bin/CHAP --config $config 2>&1
+fi
