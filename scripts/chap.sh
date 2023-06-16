4e72bad8b2b57c93e1a9bce8e4b17892ec32c4fe
Valentin Kuznetsov <vkuznet@gmail.com>
2023-06-16 11:09:34 -0400

    Add proper printouts

--- scripts/chap.sh
+++ scripts/chap.sh
@@ -7,13 +7,16 @@ profile=$4
 #echo "+++ config $config"
 #echo "+++ chapdir $chapdir"
 #echo "+++ userdir $userdir"
+#echo "+++ profile $profile"
 export PYTHONPATH=$chapdir/lib/python$pyver/site-packages:$chapdir/lib64/python$pyver/site-packages:$userdir
 export PYTHONPATH=$PYTHONPATH:$chapdir/venv/lib/python$pyver/site-packages
 #python -V
 #python -c "from users import *; print(UserProcessor)"
 #echo "#### start CHAP with $PYTHONPATH"
 if [ -n "$profile" ]; then
+    echo "run profile code"
     $chapdir/bin/CHAP --profile --config $config 2>&1
 else
+    echo "run non-profiled code"
     $chapdir/bin/CHAP --config $config 2>&1
 fi
