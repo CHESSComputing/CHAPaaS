3839c48d5a60eb4977be90f8f74441c7b669f1be
Valentin Kuznetsov <vkuznet@gmail.com>
2023-07-10 14:22:16 -0400

    update publish script

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,20 +1,29 @@
 #!/bin/bash
 #
 # check user input
-if [ $# -ne 3 ]; then
-    echo "Not enough arguments, usage: publish.sh <notebook> <user-dir> <user-repo>"
+if [ $# -ne 2 ]; then
+    echo "Not enough arguments, usage: publish.sh <user-dir> <user-repo>"
     exit 1;
 fi
 
-notebook=$1
-userDir=$2 # e.g /path/chap/Users
-userRepo=$3 # e.g /path/CHAPUsers
-echo "Publish $notebook"
+userDir=$1 # e.g /path/chap/Users/user-name
+userRepo=$2 # e.g /path/CHAPUsers
+echo "Publish:"
+echo "$userDir"
+echo "to github repo:"
+echo "$userRepo"
 cd $userDir
+
 # rsync commands
 rsync --progress -avuz --numeric-ids --exclude=__pycache__ "$userDir" "$userRepo"
 cd $userRepo
-# git commands
-#git commit -m "CHAPBook publish update"
-echo "git commit -m \"CHAPBook publish update\" ."
-cd -
+# git commands, commit only if we have git access
+user=`echo %userDir | awk '{split($1,a,"/"); print a[length(a)]}'`
+gitAccess=`cat .git/config | grep url | awk '{print $1,$2,$3}' | grep ^url | grep "git@github"`
+echo "Update code for user: $user"
+if [ -n "$gitAccess" ]; then
+    echo "commit to github"
+    git commit -m "CHAPBook publish update for $user" $user
+else
+    echo "skip github commit due to lack of protocol permission"
+fi
