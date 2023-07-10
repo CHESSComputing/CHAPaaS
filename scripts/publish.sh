f8b8db23a1b8b75e082b2699606946930d38e36c
Valentin Kuznetsov <vkuznet@gmail.com>
2023-07-10 11:44:02 -0400

    First implementation of publish script

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
+#
+# check user input
+if [ $# -ne 3 ]; then
+    echo "Not enough arguments, usage: publish.sh <notebook> <user-dir> <user-repo>"
+    exit 1;
+fi
+
 notebook=$1
-# TODO: we need to implement business logic of this script
-# - check/get access token to remote github/gitlab repo
-# - check/create new user area
-# - commit user notebook code to its area
+userDir=$2 # e.g /path/chap/Users
+userRepo=$3 # e.g /path/CHAPUsers
 echo "Publish $notebook"
+cd $userDir
+# rsync commands
+rsync --progress -avuz --numeric-ids --exclude=__pycache__ "$userDir" "$userRepo"
+cd $userRepo
+# git commands
+#git commit -m "CHAPBook publish update"
+echo "git commit -m \"CHAPBook publish update\" ."
+cd -
