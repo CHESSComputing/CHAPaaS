d6e4e14feeb331ae39d7e0bddc16ca9c1ad509ee
Valentin Kuznetsov <vkuznet@gmail.com>
2023-07-14 14:06:32 -0400

    Add url

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -2,14 +2,14 @@
 #
 # check user input
 if [ $# -ne 4 ]; then
-    echo "Not enough arguments, usage: publish.sh <user-repo> <release-tag> <release-notes> <token>"
+    echo "Not enough arguments, usage: publish.sh <user-repo> <token> <release-tag> <release-notes>"
     exit 1;
 fi
 
 dir=$1   # e.g /path/CHAPUsers/CHAPBook
-tag=$2   # e.g. v0.0.0
-notes=$3 # e.g. "some release notes"
-token=$4 # github access token string
+token=$2 # github access token string
+tag=$3   # e.g. v0.0.0
+notes=$4 # e.g. "some release notes"
 
 if [ -z "$token" ]; then
     echo "Please provide proper github access token for CHAPUsers/CHAPBook repo"
@@ -18,15 +18,18 @@ fi
 
 repo="CHAPBook"
 owner="CHAPUsers"
+url=https://api.github.com/repos/$owner/$repo/releases
+
 echo "Create release:"
 echo "Directory     : $dir"
 echo "Tag           : $tag"
 echo "Notes         : $notes"
+echo "URL           : $url"
 
 # if tag==0 then we'll grab the last available tag and increment it by 1
 if [ "$tag" == "0" ]; then
     # capture last available tag
-    lastTag=`curl -ks -H "Authorization: Bearer $token" https://api.github.com/repos/CHAPUsers/CHAPBook/releases | grep tag_name | awk '{print $2}' | sort | tail -1 | sed -e "s,\",,g" -e  "s#,##g"`
+    lastTag=`curl -ks -H "Accept: application/vnd.github+json" -H "Authorization: Bearer $token" $url | grep tag_name | awk '{print $2}' | sort | tail -1 | sed -e "s,\",,g" -e  "s#,##g"`
     echo "Last tag      : $lastTag"
     mainVersion=`echo $lastTag | awk '{split($1,a,"."); print a[1]}'`
     majorNumber=`echo $lastTag | awk '{split($1,a,"."); print a[2]}'`
@@ -48,4 +51,4 @@ curl -k -s -L \
   -H "Authorization: Bearer $token"\
   -H "X-GitHub-Api-Version: 2022-11-28" \
   -d "$payload" \
-  https://api.github.com/repos/$owner/$repo/releases
+  $url
