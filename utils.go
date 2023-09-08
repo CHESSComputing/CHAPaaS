8bd810fa5a3fe689b4726d7d1c8dcf8bce545edc
Valentin Kuznetsov <vkuznet@gmail.com>
2023-09-08 09:33:00 -0400

    Add Tar/Gzip functions

--- utils.go
+++ utils.go
@@ -6,6 +6,8 @@ package main
 //
 
 import (
+	"archive/tar"
+	"compress/gzip"
 	"crypto/tls"
 	"crypto/x509"
 	"fmt"
@@ -148,4 +150,83 @@ func getToken() string {
 func getDOI() string {
 	doi := Config.DOI
 	return fmt.Sprintf("<a href=\"https://zenodo.org/badge/latestdoi/%s\"><img src=\"https://zenodo.org/badge/%s.svg\" alt=\"DOI\"></a>", doi, doi)
+}
+
+// Tar function creates tar ball from the source and target
+// https://golangdocs.com/tar-gzip-in-golang
+func Tar(source, target string) error {
+	filename := filepath.Base(source)
+	target = filepath.Join(target, fmt.Sprintf("%s.tar", filename))
+	tarfile, err := os.Create(target)
+	if err != nil {
+		return err
+	}
+	defer tarfile.Close()
+
+	tarball := tar.NewWriter(tarfile)
+	defer tarball.Close()
+
+	info, err := os.Stat(source)
+	if err != nil {
+		return nil
+	}
+
+	var baseDir string
+	if info.IsDir() {
+		baseDir = filepath.Base(source)
+	}
+
+	return filepath.Walk(source,
+		func(path string, info os.FileInfo, err error) error {
+			if err != nil {
+				return err
+			}
+			header, err := tar.FileInfoHeader(info, info.Name())
+			if err != nil {
+				return err
+			}
+
+			if baseDir != "" {
+				header.Name = filepath.Join(baseDir, strings.TrimPrefix(path, source))
+			}
+
+			if err := tarball.WriteHeader(header); err != nil {
+				return err
+			}
+
+			if info.IsDir() {
+				return nil
+			}
+
+			file, err := os.Open(path)
+			if err != nil {
+				return err
+			}
+			defer file.Close()
+			_, err = io.Copy(tarball, file)
+			return err
+		})
+}
+
+// Gzip function gzip given source and produce output at target destination
+func Gzip(source, target string) error {
+	reader, err := os.Open(source)
+	if err != nil {
+		return err
+	}
+
+	filename := filepath.Base(source)
+	target = filepath.Join(target, fmt.Sprintf("%s.gz", filename))
+	writer, err := os.Create(target)
+	if err != nil {
+		return err
+	}
+	defer writer.Close()
+
+	archiver := gzip.NewWriter(writer)
+	archiver.Name = filename
+	defer archiver.Close()
+
+	_, err = io.Copy(archiver, reader)
+	return err
 }
